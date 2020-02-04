daa071f0d9664884e61f42310e3f6e7944de9873
Yeow Jing Jong <yeowjingjong@gmail.com>
2020-02-04 15:18:04 +0800

    tomato feature, make everything uppercase

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ def main():
 
 main()
 
+<<<<<<< HEAD
 
 def watermelon():
     print("watermelon commit #1")
     print("watermelon commit #2")
 
 watermelon()
+=======
+def tomato():
+    print("Second commit:" main().upper)
+>>>>>>> tomato feature, make everything uppercase
