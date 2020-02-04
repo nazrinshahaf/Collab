f797d3d6708f4438fdbb91279302eb18205c9fae
Sebastien Jurkowski <sebastienjurkowski@gmail.com>
2020-02-04 16:03:05 +0800

    fixed it all

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
 def main():
     print("first commit")
-<<<<<<< HEAD
     print("naz is da boss")
-=======
     print('yoohoo seb')
->>>>>>> added something into main
 
 main()
 
-<<<<<<< HEAD
 
 def watermelon():
     print("watermelon commit #1")
     print("watermelon commit #2")
 
 watermelon()
-=======
 def tomato():
-    print("Second commit:" main().upper)
->>>>>>> tomato feature, make everything uppercase
+    print("Second commit:")
